a5f634dfad4f27e978dd85ab63291fbcf454feaf
Paul Tagliamonte <tag@pault.ag>
2015-09-30 17:49:53 -0400

    Fiddle with local trains API

--- proxy.js
+++ proxy.js
@@ -22,7 +22,7 @@ const Gio = imports.gi.Gio;
 const Lang = imports.lang;
 const GLib = imports.gi.GLib;
 
-const WMATAInterface = '<node name="/org/anized/wmata"><interface name="org.anized.wmata.Rail"><method name="NextTrains"><arg type="as" direction="in"></arg><arg type="aa{ss}" direction="out"></arg></method></interface><interface name="org.freedesktop.DBus.Introspectable"><method name="Introspect"><arg name="out" type="s" direction="out"></arg></method></interface></node>';
+const WMATAInterface = '<node name="/org/anized/wmata"><interface name="org.anized.wmata.Rail"><method name="NextLocalTrains"><arg type="aa{ss}" direction="out"></arg></method><method name="NextTrains"><arg type="as" direction="in"></arg><arg type="aa{ss}" direction="out"></arg></method></interface><interface name="org.freedesktop.DBus.Introspectable"><method name="Introspect"><arg name="out" type="s" direction="out"></arg></method></interface></node>';
 
 
 /**
@@ -52,11 +52,24 @@ const WMATARailProxy = new Lang.Class({
             ret.error = e;
         }
         return ret;
+    },
+
+    /**
+     * Get the next trains for the stop that the wmatad thinks we're after
+     * by guessing against the network access points sitting around us. */
+    GetNextLocalTrains: function() {
+        var ret = {"predictions": [], "error": null};
+        try {
+            ret.predictions = this.proxy.NextLocalTrainsSync()[0];
+        } catch (e) {
+            ret.error = e;
+        }
+        return ret;
     }
 });
 
 // var railProxy = new WMATARailProxy();
-// log(railProxy.GetNextTrains(["A03"]).predictions.map(function(e) {
+// log(railProxy.GetNextLocalTrains().predictions.map(function(e) {
 //     return e.cars + ", " + e.desitnation;
 // }));
 
